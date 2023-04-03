75e3036cee674974d7b4f1c5bb4f994476523e6d
sehui <102664819+birdhui@users.noreply.github.com>
2023-04-03 22:31:19 +0900

    Add files via upload

---
+++ 0328_class.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+struct S
+{
+	int x;
+};
+
+int main()
+{
+	S s;
+	s.x = 1234;
+
+	printf("%d", s.x);
+
+	return 0;
+}
+*/
+
+class C
+{
+	int x;
+};
+
+int main()
+{
+	C c;
+	c.x = 1234;
+
+	printf("%d", c.x);
+	return 0;
+}
