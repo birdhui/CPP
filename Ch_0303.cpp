19fe194c82c9b49cc9cc163742a124bab6110f55
sehui <102664819+birdhui@users.noreply.github.com>
2023-04-18 00:03:08 +0900

    Week 3 Review
    Week 3 Review

---
+++ Ch_0303.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char name[10];
+
+	cout << "이름을 입력하세요 >> ";
+	cin >> name;
+
+	cout << "이름은 " << name << "입니다." << endl;
+
+	return 0;
+}
